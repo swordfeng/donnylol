b027ffe1555e47cc1ec599ef9c0c579504c7b94f
Jianfeng Zhang <swordfeng123@gmail.com>
2021-03-13 22:53:45 -0800

    remove (useless) debug flag

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
 import { handleRequest } from './handler'
 
-const DEBUG = true
-
 addEventListener('fetch', (event) => {
     try {
         event.respondWith(handleRequest(event.request))
     } catch (e) {
-        if (DEBUG) {
-            return event.respondWith(
-                new Response(e.message || e.toString(), {
-                    status: 500,
-                }),
-            )
-        }
         event.respondWith(new Response('Internal Error', { status: 500 }))
     }
 })
