8760d6d3494f9632f9fda8c9e8ed55f69fa92e39
Jianfeng Zhang <swordfeng123@gmail.com>
2021-03-20 01:26:44 -0700

    show errors

--- src/sw.ts
+++ src/sw.ts
@@ -2,7 +2,7 @@ import { handleRequest, syncRules } from './swHandler'
 
 self.addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request).catch(e => {
-        console.log(`Failed to handle request ${event.request.url}: ${e}`)
+        console.error(`Failed to handle request ${event.request.url}:`, e)
         return fetch(event.request)
     }))
 })
