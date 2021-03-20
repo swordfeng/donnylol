7a73071ca3f4204531edf2075a0e506d4a10349b
Jianfeng Zhang <swordfeng123@gmail.com>
2021-03-19 23:50:07 -0700

    fxxk

--- static/sw.js
+++ static/sw.js
@@ -1,1 +1,246 @@
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});const r=n(1);self.addEventListener("fetch",e=>{try{e.respondWith(r.handleRequest(e.request))}catch(t){e.respondWith(fetch(e.request))}}),self.addEventListener("sync",e=>{e.waitUntil(r.syncRules())})},function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.syncRules=t.handleRequest=void 0;const r=n(2);async function o(){const e=new Request("/rules.json"),t=await fetch(e);if(!t.ok)throw new Error("no rules available");const n=new Headers;n.set("cache-control","public, max-age=604800, immutable");const r=new Response(t.body,{status:t.status,statusText:t.statusText,headers:n});return caches.open("rules").then(t=>t.put(e,r)),t}t.handleRequest=async function(e){const t=new URL(e.url);if("/"===t.pathname){const e=await async function(){let e=await caches.match(new Request("/rules.json"));e||(e=await o());return e.json()}(),n=t.searchParams.get("q");if(n){const t=r.handleQuery(n,e);if(t)return Response.redirect(t,302)}}return fetch(e)},t.syncRules=o},function(e,t,n){"use strict";function r(e,t){const n=e.indexOf(" "),r=-1===n?e:e.slice(0,n),o=-1===n?"":e.slice(n+1);for(const n of t){if(n.keywords&&-1!==n.keywords.indexOf(r)&&(o||n.default))return{rule:n,keyword:r,remainQuery:o};if(n.match){const t=new RegExp("^(?:"+n.match+")(?=$| )").exec(e);if(t){const r=e.slice(t[0].length+1);if(r||n.default)return{rule:n,keyword:t[0],remainQuery:r,match:t}}}if(!n.keywords&&!n.match)return{rule:n,keyword:"",remainQuery:e}}return null}function o(e,t,n,r){if(e=(e=e.replace(/\{keyword\}/g,encodeURIComponent(t))).replace(/\{query\}/g,encodeURIComponent(n).replace(/%20/g,"+")),r)for(let t=1;t<r.length;t++)e=e.replace(new RegExp(`\\{${t}\\}`,"g"),encodeURIComponent(r[t]));return e}Object.defineProperty(t,"__esModule",{value:!0}),t.handleSuggestion=t.handleQuery=void 0,t.handleQuery=function(e,t){const n=r(e,t);if(!n)return null;const{rule:u,keyword:s,remainQuery:c,match:a}=n;return!c&&u.default?o(u.default,s,c,a):o(u.search,s,c,a)},t.handleSuggestion=async function(e,t){const n=r(e,t);if(!n)return[e,[],[],[]];const{rule:u,keyword:s,remainQuery:c,match:a}=n;if(!c||!u.suggestion)return[e,[],[],[]];const i=o(u.suggestion,s,c,a),l=await fetch(new Request(i)),f=await l.json();return f[0]!==c?[e,[],[],[]]:(f[0]=e,f[1]=f[1].map(e=>`${s} ${e}`),f)}}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/sw.ts");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/rules.ts":
+/*!**********************!*\
+  !*** ./src/rules.ts ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handleSuggestion = exports.handleQuery = void 0;
+function matchRule(query, rules) {
+    const spaceIdx = query.indexOf(' ');
+    const keyword = spaceIdx === -1 ? query : query.slice(0, spaceIdx);
+    const remainQuery = spaceIdx === -1 ? '' : query.slice(spaceIdx + 1);
+    for (const rule of rules) {
+        if (rule.keywords && rule.keywords.indexOf(keyword) !== -1) {
+            if (remainQuery || rule.default) {
+                return { rule, keyword, remainQuery };
+            }
+        }
+        if (rule.match) {
+            const match = new RegExp('^(?:' + rule.match + ')(?=$| )').exec(query);
+            if (match) {
+                const matchedQuery = query.slice(match[0].length + 1);
+                if (matchedQuery || rule.default) {
+                    return { rule, keyword: match[0], remainQuery: matchedQuery, match };
+                }
+            }
+        }
+        if (!rule.keywords && !rule.match) {
+            return { rule, keyword: '', remainQuery: query };
+        }
+    }
+    return null;
+}
+function handleQuery(query, rules) {
+    const ruleMatch = matchRule(query, rules);
+    if (!ruleMatch)
+        return null;
+    const { rule, keyword, remainQuery, match } = ruleMatch;
+    if (!remainQuery && rule.default) {
+        return replaceUrl(rule.default, keyword, remainQuery, match);
+    }
+    return replaceUrl(rule.search, keyword, remainQuery, match);
+}
+exports.handleQuery = handleQuery;
+async function handleSuggestion(query, rules) {
+    const ruleMatch = matchRule(query, rules);
+    if (!ruleMatch)
+        return [query, [], [], []];
+    const { rule, keyword, remainQuery, match } = ruleMatch;
+    if (!remainQuery || !rule.suggestion)
+        return [query, [], [], []];
+    const suggestionUrl = replaceUrl(rule.suggestion, keyword, remainQuery, match);
+    const response = await fetch(new Request(suggestionUrl));
+    const result = await response.json();
+    if (result[0] !== remainQuery)
+        return [query, [], [], []];
+    result[0] = query;
+    result[1] = result[1].map(suggestion => `${keyword} ${suggestion}`);
+    return result;
+}
+exports.handleSuggestion = handleSuggestion;
+function replaceUrl(url, keyword, query, match) {
+    url = url.replace(/\{keyword\}/g, encodeURIComponent(keyword));
+    url = url.replace(/\{query\}/g, encodeURIComponent(query).replace(/%20/g, '+'));
+    if (match) {
+        for (let i = 1; i < match.length; i++) {
+            url = url.replace(new RegExp(`\\{${i}\\}`, 'g'), encodeURIComponent(match[i]));
+        }
+    }
+    return url;
+}
+
+
+/***/ }),
+
+/***/ "./src/sw.ts":
+/*!*******************!*\
+  !*** ./src/sw.ts ***!
+  \*******************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+const swHandler_1 = __webpack_require__(/*! ./swHandler */ "./src/swHandler.ts");
+self.addEventListener('fetch', (event) => {
+    try {
+        event.respondWith(swHandler_1.handleRequest(event.request));
+    }
+    catch (e) {
+        event.respondWith(fetch(event.request));
+    }
+});
+self.addEventListener('sync', (event) => {
+    event.waitUntil(swHandler_1.syncRules());
+});
+
+
+/***/ }),
+
+/***/ "./src/swHandler.ts":
+/*!**************************!*\
+  !*** ./src/swHandler.ts ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.syncRules = exports.handleRequest = void 0;
+const rules_1 = __webpack_require__(/*! ./rules */ "./src/rules.ts");
+const CACHE_NAME = 'rules';
+async function handleRequest(request) {
+    const request_url = new URL(request.url);
+    if (request_url.pathname === '/') {
+        const rules = await fetchRules();
+        const query = request_url.searchParams.get("q");
+        if (query) {
+            const redirect_url = rules_1.handleQuery(query, rules);
+            if (redirect_url)
+                return Response.redirect(redirect_url, 302);
+        }
+    }
+    return fetch(request);
+}
+exports.handleRequest = handleRequest;
+async function fetchRules() {
+    let response = await caches.match(new Request('/rules.json'));
+    if (!response) {
+        response = await syncRules();
+    }
+    return response.json();
+}
+async function syncRules() {
+    const request = new Request('/rules.json');
+    const response = await fetch(request);
+    if (!response.ok)
+        throw new Error('no rules available');
+    const response_cache_headers = new Headers();
+    response_cache_headers.set('cache-control', 'public, max-age=604800, immutable');
+    const response_cache = new Response(response.body, {
+        status: response.status,
+        statusText: response.statusText,
+        headers: response_cache_headers
+    });
+    caches.open(CACHE_NAME).then(cache => cache.put(request, response_cache));
+    return response;
+}
+exports.syncRules = syncRules;
+
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=sw.js.map
